4ee92497afba37153b137dfa81509e643cabad9c
JNjeong <wnsghd0902@gmail.com>
2026-07-08 17:21:53 +0900

    bad-words 라이브러리추가, 한글욕설필터링 정규식 추가, 필터링 사용법 주석으로 추가

--- public/app.js
+++ public/app.js
@@ -1,9 +1,23 @@
-import elements from "./elementCollection.js";
+import elements from "./elementCollection.js"; //전역변수 모음
+import Filter from 'bad-words' //영문 욕설 필터링
 
 
 //---------------------------------------------------------
 // 전역변ㅅ 설정
 const elementCol = elements
+const filter = new Filter()
+
+function filterEng(text){
+    /*
+    filter.clean(text): ***로 치환
+    filter.isProfane(text): 있으면 true/ 없으면 false
+        > .isProfane 으로 true값 반환되면 글자수만큼 하트로 대체하는건 수작업 필요
+    
+    filter.addWords("필터링할단어"): 필터링할 단어 추가
+    filter.removeWords("필터링안할단어"): 해당단어 필터링안함
+    
+    */
+}
 
 
 
