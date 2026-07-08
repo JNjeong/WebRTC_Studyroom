8bbdf5b0436a84ac225fae66a03633456936eae0
JNjeong <wnsghd0902@gmail.com>
2026-07-08 16:34:38 +0900

    element Collection 파일생성

--- public/elementCollection.js
+++ public/elementCollection.js
@@ -2,23 +2,5 @@
 
 export default element = {
     "loginBox" : document.getElementById("loginBox"),
-    "mainBox" : document.getElementById("mainBox"),
-    "nicknameInput" : document.getElementById("nicknameInput"),
-    "ageInput" : document.getElementById("ageInput"),
-    "joinBtn" : document.getElementById("joinBtn"),
-    "findBtn" : document.getElementById("findBtn"),
-    "nextBtn" : document.getElementById("nextBtn"),
-    "hangupBtn" : document.getElementById("hangupBtn"),
-    "micBtn" : document.getElementById("micBtn"),
-    "cameraBtn" : document.getElementById("cameraBtn"),
-    "myName" : document.getElementById("myName"),
-    "statusText" : document.getElementById("statusText"),
-    "partnerLabel" : document.getElementById("partnerLabel"),
-    "localVideo" : document.getElementById("localVideo"),
-    "remoteVideo" : document.getElementById("remoteVideo"),
-    "chatMessages" : document.getElementById("chatMessages"),
-    "chatForm" : document.getElementById("chatForm"),
-    "chatInput" : document.getElementById("chatInput"),
-    "sendBtn" : document.getElementById("sendBtn")
-
+    
     }
