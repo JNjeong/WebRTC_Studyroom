12a7f2b3903c508812cfe6d2e71c16988a8d5dc5
JNjeong <wnsghd0902@gmail.com>
2026-07-08 10:38:45 +0900

    프로젝트 초기화

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import expess from "express"
+import http from "http"
+import {Server} from "socket.io"
+import path from "path"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = 3000
+server.listem(PORT, ()=>{
+    console.log('server running')
+})
