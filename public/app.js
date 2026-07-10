a0a8e115075a887106490652bb3dad6bb69072e4
angela5825 <99yoolee@naver.com>
2026-07-10 22:01:19 +0900

    webRTC- offer  생성 및 처리 함수 추가

--- public/app.js
+++ public/app.js
@@ -1,13 +1,18 @@
 import elements from "./elementCollection.js"; //전역변수 모음
 import Filter from 'bad-words' //영문 욕설 필터링
-import rtcConfig from "../config/stun.js" ; // webRTC STUN 설정 
+import {rtcConfig} from "../config/stun.js" ; // webRTC STUN 설정 
 
 //---------------------------------------------------------
 // 전역변ㅅ 설정
 const elementCol = elements
 const filter = new Filter()
 
+const socket = io();
+
 let localStream = null // 카메라/마이크 실행 -> MediaStream 객체 생성 
+let peerConnection =null // webRTC 연결 객체 
+
+let roomId= null // 방번호
 
 function filterEng(text){
     /*
@@ -52,9 +57,12 @@ function createPeerConnection(){
     peerConnection = new RTCPeerConnection(rtcConfig)
 
     // 2) 사용자의 track 등록하기 
-    localStream.getTracks().forEach((track)=>{
-        peerConnection.addTrack(track, localStream)
-    })
+    if(localStream){
+        localStream.getTracks().forEach((track)=>{
+            peerConnection.addTrack(track, localStream)
+        })
+    }
+    
 
     // 3) 상대의 영상/음성 track 오면 실행
     peerConnection.ontrack = (e)=>{
@@ -78,6 +86,42 @@ function createPeerConnection(){
     }
 }
 
+// offer 생성하는 함수 
+async function makeOffer(){
+
+    if (!localStream) { 
+        await startLocalMedia();
+      }
+
+    if(!peerConnection){
+        createPeerConnection()
+         
+    }
+
+    const offer = await peerConnection.createOffer()
+
+    await peerConnection.setLocalDescription(offer)
+    socket.emit('offer',{roomId, offer}) // 생성한 Offer를 서버를 통해 상대방에게 전송
+
+    
+}
+
+// 상대방이 보낸 offer을 PeerConnection의 remoteDescription으로 등록
+async function handleOffer(offer){
+
+    if(!localStream){
+        await startLocalMedia()
+    }
+
+    if(!peerConnection) createPeerConnection() // peerConnection 만들어지지 않았으면 생성하기 
+
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer)) // 상대방의 offer를 내 브라우저에 저장 
+
+    const answer = await peerConnection.createAnswer() // 상대방의 offer에 대한 answer 생성
+    await peerConnection.setLocalDescription(answer)
+    socket.emit('answer',{roomId,answer}) // 서버를 통해 사용자의 answer을 상대방에게 보내기 
+}
+
 // webRTC 연결 종료 및 초기화하는 함수 
 function resetPeerConnection() {
     if (peerConnection) {
@@ -92,3 +136,16 @@ function resetPeerConnection() {
 
 // ---------------------------------------------------------
 // socket 설정
+
+
+
+
+//----------------------------------------------------
+/* WebRTC 관련  */
+
+socket.on('offer',async({offer})=>{ // 상대방으로부터 offer을 받았을 때 handleOffer() 함수 실행
+    await handleOffer(offer)
+})
+
+
+//----------------------------------------------------
